709a403da1dc3f5d7250658f6d898d7fd6791f7b
Ilya Belenkiy <ilya@harmonicsense.com>
2021-08-05 21:32:31 -0400

    Remove iOS 15 color extensions and refactor measurement

--- Sources/SwiftUIEx/Layout.swift
+++ Sources/SwiftUIEx/Layout.swift
@@ -14,12 +14,20 @@ public struct MeasurementKey<Value, Tag>: PreferenceKey {
     }
 }
 
-public extension View {
-    func measurement<Value, Tag>(_ keyType: MeasurementKey<Value, Tag>.Type, _ eval: @escaping (GeometryProxy) -> Value?) -> some View {
-        background(
+public struct Measurement<Value, Tag>: ViewModifier {
+    let eval: (GeometryProxy) -> Value?
+
+    public func body(content: Content) -> some View {
+        content.background(
             GeometryReader { proxy in
                 Color.clear.preference(key: MeasurementKey<Value, Tag>.self, value: eval(proxy))
             }
         )
+    }
+}
+
+public extension View {
+    func measurement<Value, Tag>(_ keyType: MeasurementKey<Value, Tag>.Type, _ eval: @escaping (GeometryProxy) -> Value?) -> some View {
+        modifier(Measurement<Value, Tag>(eval: eval))
     }
 }
