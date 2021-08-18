75e5bcd14067b54dc83a6642e6ddf4197f776b0b
Ilya Belenkiy <ilya@harmonicsense.com>
2021-08-17 21:25:52 -0400

    Improve Collection layout

--- Sources/SwiftUIEx/Collection.swift
+++ Sources/SwiftUIEx/Collection.swift
@@ -32,43 +32,90 @@ extension CollectionCell {
 }
 
 public struct Collection<Cell: CollectionCell>: View {
+    enum ContentWidthTag {}
+    typealias ContentWidthKey = MeasurementKey<CGFloat, ContentWidthTag>
+
+    public enum CellWidth {
+        case fixed(CGFloat)
+        case min(CGFloat)
+    }
+
     public typealias T = Cell.T
     public let content: [T]
     public var selection: Binding<Selection<T>?>
 
-    public let columnCount: Int
-    public let cellMinWidth: CGFloat
-    public var spacing: CGFloat?
+    public let cellWidth: CellWidth
+    public let columnCount: Int?
+    public let columnSpacing: CGFloat?
+    public let rowSpacing: CGFloat?
+
+    private let gridItem: GridItem
+
+    @State private var contentWidth: CGFloat?
+
+    func evalContentWidth(availableWidth: CGFloat) -> CGFloat? {
+        func width(columnCount: Int, cellWidth: CGFloat) -> CGFloat {
+            CGFloat(columnCount) * cellWidth + CGFloat(columnCount - 1) * (columnSpacing ?? 0)
+        }
+
+        switch cellWidth {
+        case .fixed(let value):
+            if let count = columnCount {
+                return width(columnCount: count, cellWidth: value)                
+            }
+            else {
+                let w = availableWidth
+                let d1 = value
+                let d2 = columnSpacing ?? 0
+                // c * d1 + (c-1) * d2 <= w
+                return width(columnCount: Int(floor((w + d2) / (d1 + d2))), cellWidth: value)
+            }
+        case .min:
+            return nil
+        }
+    }
 
     public init(
-        columnCount: Int = 1,
         content: [T],
         selection: Binding<Selection<T>?>,
-        cellMinWidth: CGFloat = minTappableRectSide,
-        spacing: CGFloat? = nil
+        columnCount: Int? = nil,
+        columnSpacing: CGFloat? = nil,
+        rowSpacing: CGFloat? = nil,
+        cellWidth: CellWidth? = nil
     ) {
-        self.columnCount = columnCount
         self.content = content
         self.selection = selection
-        self.cellMinWidth = cellMinWidth
-        self.spacing = spacing
+
+        self.cellWidth = cellWidth ?? .min(minTappableRectSide)
+        self.columnCount = columnCount
+        self.columnSpacing = columnSpacing
+        self.rowSpacing = rowSpacing
+
+        switch self.cellWidth {
+        case .fixed(let value):
+            gridItem = GridItem(
+                columnCount != nil ? .fixed(value) : .adaptive(minimum: value, maximum: value),
+                spacing: columnSpacing
+            )
+
+        case .min(let value):
+            gridItem = GridItem(
+                columnCount != nil ? .flexible(minimum: value) : .adaptive(minimum: value),
+                spacing: columnSpacing
+            )
+        }
     }
 
     public var body: some View {
-        if columnCount == 1 {
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: cellMinWidth))]) {
-                ForEach(content.indices) { index in
-                    Cell(index: index, value: content[index], selection: selection)
-                }
+        let columns = Array(repeating: gridItem, count: columnCount ?? 1)
+        LazyVGrid(columns: columns, spacing: rowSpacing) {
+            ForEach(content.indices) { index in
+                Cell(index: index, value: content[index], selection: selection)
             }
         }
-        else {
-            let columns = Array(repeating: GridItem(.flexible()), count: columnCount)
-            LazyVGrid(columns: columns) {
-                ForEach(content.indices) { index in
-                    Cell(index: index, value: content[index], selection: selection)
-                }
-            }
+        .measurement(ContentWidthKey.self) { $0.size.width }
+        .onPreferenceChange(ContentWidthKey.self) { width in
+            contentWidth = width.flatMap { evalContentWidth(availableWidth: $0) }
         }
     }
 }
