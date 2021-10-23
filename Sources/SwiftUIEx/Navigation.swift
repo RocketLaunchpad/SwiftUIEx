4519fe7aef65cab940e34ac8271dd264a31f4aa4
Ilya Belenkiy <ilya@harmonicsense.com>
2021-10-23 12:24:22 -0400

    add NavigationRow

--- Sources/SwiftUIEx/Navigation.swift
+++ Sources/SwiftUIEx/Navigation.swift
@@ -123,3 +123,34 @@ public extension NavigationItemView where NextView == EmptyView {
         self.nextView = { _, _ in EmptyView() }
     }
 }
+
+public struct NavigationRow<V: View>: View {
+    public let label: V
+    public let action: () -> Void
+
+    public init(action: @escaping () -> Void, label: @autoclosure () -> V) {
+        self.label = label()
+        self.action = action
+    }
+
+    public init(_ titleKey: LocalizedStringKey, action: @escaping () -> Void) where V == Text {
+        self.init(action: action, label: Text(titleKey))
+    }
+
+    public init(_ title: String, action: @escaping () -> Void) where V == Text {
+        self.init(action: action, label: Text(title))
+    }
+
+    public var body: some View {
+        Button(action: action) {
+            HStack {
+                label
+                Spacer()
+                Image(systemName: "chevron.right")
+                    .foregroundColor(.secondary)
+            }
+            .contentShape(Rectangle())
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+}
