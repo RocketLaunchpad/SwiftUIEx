8162c5f331d286a308811aca2317c69aed466f5a
Ilya Belenkiy <ilya@harmonicsense.com>
2021-11-17 21:49:12 -0500

    Add PlainNavigationLink

---
+++ Sources/SwiftUIEx/PlainNavigationLink.swift
@@ -0,0 +1,30 @@
+//
+//  PlainNavigationLink.swift
+//  Rocket Insights
+//
+//  Created by Ilya Belenkiy on 11/17/21.
+//
+
+import Foundation
+import SwiftUI
+
+public struct PlainNavigationLink<Label: View, Destination: View>: View {
+    let label: () -> Label
+    let destination: () -> Destination
+
+    @State private var linkIsActive = false
+
+    public init(destination: @autoclosure @escaping () -> Destination, label: @escaping () -> Label) {
+        self.label = label
+        self.destination = destination
+    }
+
+    public var body: some View {
+        VStack(spacing: 0) {
+            label().onTapGesture {
+                linkIsActive = true
+            }
+            NavigationLink(isActive: $linkIsActive, destination: destination, label: { EmptyView() })
+        }
+    }
+}
