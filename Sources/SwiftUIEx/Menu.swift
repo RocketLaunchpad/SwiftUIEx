925ae153ef9e3a02eafd49ec91c060149a3b37c3
Ilya Belenkiy <ilya@harmonicsense.com>
2022-02-17 22:36:03 -0500

    Add MenuButton
    The native Menu from SwiftUI has layout bugs that corrupt the layout so that other UI elements become unresponsive.

--- Sources/SwiftUIEx/Menu.swift
+++ Sources/SwiftUIEx/Menu.swift
@@ -7,29 +7,94 @@
 
 import SwiftUI
 
-public func menuItem(destructive: Bool = false, text: String, iconName: String? = nil, action: @escaping () -> Void) -> some View {
-    if #available(iOS 15, *) {
-        return Button(
-            role: destructive ? .destructive : nil,
-            action: action,
-            label: {
-                HStack {
-                    Text(text)
-                    if let iconName = iconName {
-                        Image(systemName: iconName)
-                    }
-                }
-            }
-        )
+public func menuItem(
+    destructive: Bool = false,
+    text: String, iconName: String? = nil,
+    disabled: Bool = false,
+    action: @escaping () -> Void
+)
+-> UIAction
+{
+    var attributes: UIMenuElement.Attributes = []
+    if destructive {
+        attributes.insert(.destructive)
     }
-    else {
-        return Button(action: action) {
-            HStack {
-                Text(text)
-                if let iconName = iconName {
-                    Image(systemName: iconName)
-                }
+    if disabled {
+        attributes.insert(.disabled)
+    }
+
+    return .init(
+        title: text,
+        image: iconName.flatMap { UIImage(systemName: $0) },
+        attributes: attributes,
+        handler: { _ in action() }
+    )
+}
+
+public func submenuItem(text: String, iconName: String? = nil, items: [UIMenuElement]) -> UIMenu {
+    .init(
+        title: text,
+        image: iconName.flatMap { UIImage(systemName: $0) },
+        children: items
+    )
+}
+
+public struct MenuButton: UIViewRepresentable {
+    let title: String
+    let image: UIImage?
+    let color: UIColor
+
+    let items: [UIMenuElement]
+
+    public init(
+        title: String = "",
+        imageSystemName: String? = nil,
+        imageName: String? = nil,
+        color: Color = Color.accentColor,
+        fontSize: CGFloat? = nil,
+        items: [UIMenuElement]
+    ) {
+        self.title = title
+
+        if let name = imageSystemName {
+            let config: UIImage.SymbolConfiguration?
+            if let fontSize = fontSize {
+                config = UIImage.SymbolConfiguration(pointSize: fontSize, weight: .regular)
             }
+            else {
+                config = nil
+            }
+            image = UIImage(systemName: name, withConfiguration: config)
         }
+        else if let name = imageName {
+            image = UIImage(named: name)
+        }
+        else {
+            self.image = nil
+        }
+
+        self.color = .init(color)
+        self.items = items
+    }
+
+    public func makeUIView(context: Context) -> some UIView {
+        let button = UIButton()
+        button.setContentHuggingPriority(.required, for: .horizontal)
+        button.setContentHuggingPriority(.required, for: .vertical)
+
+        button.setTitleColor(color, for: .normal)
+        if !title.isEmpty {
+            button.setTitle(title, for: .normal)
+        }
+        button.setImage(image, for: .normal)
+        button.tintColor = color
+
+        button.menu = UIMenu(children: items)
+        button.showsMenuAsPrimaryAction = true
+
+        return button
+    }
+
+    public func updateUIView(_ uiView: UIViewType, context: Context) {
     }
 }
