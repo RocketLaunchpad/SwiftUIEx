013a96dc94f1805cb24d72d9e6eda97209d27e2b
Ilya Belenkiy <ilya@harmonicsense.com>
2022-05-02 22:18:13 -0400

    Add an option to disable PlainNavigationLink so that there is no action when tapping on it

--- Sources/SwiftUIEx/PlainNavigationLink.swift
+++ Sources/SwiftUIEx/PlainNavigationLink.swift
@@ -10,21 +10,26 @@ import SwiftUI
 
 public struct PlainNavigationLink<Label: View, Destination: View>: View {
     let label: () -> Label
+    let enabled: Bool
     let destination: () -> Destination
 
     @State private var linkIsActive = false
 
-    public init(destination: @autoclosure @escaping () -> Destination, @ViewBuilder label: @escaping () -> Label) {
+    public init(destination: @autoclosure @escaping () -> Destination, enabled: Bool = true, @ViewBuilder label: @escaping () -> Label) {
         self.label = label
+        self.enabled = enabled
         self.destination = destination
     }
 
     public var body: some View {
         VStack(spacing: 0) {
             label().onTapGesture {
+                guard enabled else { return }
                 linkIsActive = true
             }
-            NavigationLink(isActive: $linkIsActive, destination: destination, label: { EmptyView() })
+            if enabled {
+                NavigationLink(isActive: $linkIsActive, destination: destination, label: { EmptyView() })
+            }
         }
     }
 }
